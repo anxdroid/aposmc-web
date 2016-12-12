d6e484c65690b063ba2be1990946b48aac9ba7ae
Antonello Paoletti <antonello.paoletti@lnf.infn.it>
2016-12-12 15:37:07 +0100

    modifiche

--- index_mysql.php
+++ index_mysql.php
@@ -216,7 +216,7 @@ error_reporting(E_ALL);
 	$area = 0;
 
 	if ($cumulative) {
-		ksort($seriers);
+		krsort($seriers);
 		foreach($series as $ts => $row) {
 			if ($prevVal != null) {
 				$timeDiff = abs(strtotime($ts) - $prevTs);
